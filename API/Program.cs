6de2d467b17ba7053c0f5a603d78a1ae8190eb81
Luka Tomić <61879344+tomicluka24@users.noreply.github.com>
2022-05-03 11:43:02 +0200

    Validation / Authentication

--- API/Program.cs
+++ API/Program.cs
@@ -1,19 +1,48 @@
- var builder = WebApplication.CreateBuilder(args);
+using System.Text;
+using API.Data;
+using API.Extensions;
+using API.Interfaces;
+using API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
-    
-    // Add services to the container.
+var builder = WebApplication.CreateBuilder(args);
     
     builder.Services.AddControllers();
-    
-    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
     builder.Services.AddEndpointsApiExplorer();
     builder.Services.AddSwaggerGen();
-    //services cors
+    builder.Services.AddApplicationServices(builder.Configuration);
+    // builder.Services.AddScoped<ITokenService, TokenService>();
+    // builder.Services.AddDbContext<DataContext>(options =>
+    //         {
+    //             options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+    //         });
     builder.Services.AddCors(p => p.AddPolicy("corsapp", builder =>
     {
         builder.WithOrigins("https://localhost:4200").AllowAnyMethod().AllowAnyHeader();
     }));
+    builder.Services.AddIdentityServices(builder.Configuration);
+
+    // builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options => 
+    // {
+    //     options.TokenValidationParameters = new TokenValidationParameters
+    //     {
+    //         ValidateIssuerSigningKey = true,
+    //         IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["TokenKey"])),
+    //         ValidateIssuer = false,
+    //         ValidateAudience = false
+    //     };
+    // });
     
+
+
+
+
+
+
+
+
     var app = builder.Build();
     
     if (app.Environment.IsDevelopment())
@@ -25,6 +54,7 @@
        //app cors
         app.UseCors("corsapp");
         app.UseHttpsRedirection();
+        app.UseAuthentication();
         app.UseAuthorization();
         //app.UseCors(prodCorsPolicy);
     
